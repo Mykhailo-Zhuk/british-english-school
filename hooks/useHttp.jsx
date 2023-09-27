60c08a792da1c6a2194110f0323955474127a550
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2023-09-27 22:35:03 +0300

    change url

--- hooks/useHttp.jsx
+++ hooks/useHttp.jsx
@@ -3,9 +3,8 @@
 import axios from 'axios';
 import { useCallback, useState } from 'react';
 
-const baseURL =
-  // 'https://british-english-school.vercel.app/api/';
-  'http://localhost:3000/api/';
+const baseURL = 'https://british-english-school.vercel.app/api/';
+// 'http://localhost:3000/api/';
 
 const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
